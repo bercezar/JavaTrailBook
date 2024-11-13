fbfbc38227bda54f5de266284364700dee31acaa
Bernardo Cezar <b.cezar1812@gmail.com>
2024-11-13 12:46:04 -0300

    added gson

--- Java/spring-boot-java/src/main/java/com/spring/spring_boot_java/MyApp.java
+++ Java/spring-boot-java/src/main/java/com/spring/spring_boot_java/MyApp.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class MyApp implements CommandLineRunner {
     @Autowired
