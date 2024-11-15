1a94ffccc98f53a128e774cbd8bd2822109674b3
Bernardo Cezar <b.cezar1812@gmail.com>
2024-11-15 19:58:54 -0300

    update spring folder

--- Java/spring-boot-java/src/main/java/com/spring/beansVScomponents/Beans.java
+++ Java/spring-boot-java/src/main/java/com/spring/beansVScomponents/Beans.java
@@ -4,9 +4,10 @@ import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // Classe de configuração do Spring. Ao spring, os métodos definidos dentro dela podem ser usados para criar e configurar beans
 public class Beans {
-    @Bean
+    // A classe registra um bean do tipo Gson.
+    @Bean // Será genrenciado pelo framework Spring boot
     public Gson gson(){
         return new Gson();
     }
